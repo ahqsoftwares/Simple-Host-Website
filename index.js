e7ed659050294a633f5dd0249f1a08345a29e3e1
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-06-07 20:49:03 +0530

    test

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require("express");
 const app = express();
 
 app.listen(1876);
+app.use("ejs");
+app.use("views", path.join(__dirname + "pages"));
+
+app.get("/", async(req, res) => {
+         res.render(`main.ejs`);
+});
