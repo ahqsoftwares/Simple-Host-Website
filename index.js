4c9448672ea70c6e4aa63713f4eaab2f5cf04dae
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-06-07 23:34:02 +0530

    loading page

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const client = new (require("eris"))(process.env.token, {
-         intents: ["guilds", "guildMembers", ]
-});
-client.connect();
+// const client = new (require("eris"))(process.env.token, {
+//          intents: ["guilds", "guildMembers", ]
+// });
+// client.connect();
 const path = require("path");
 const { get } = require("./modules/user");
 
@@ -13,13 +13,27 @@ app.listen(1876);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "pages"));
 app.use((req, res, next) => {
-         req.user = get[req.headers[`x-forwarded-for`]];
-         next()
+         try {
+                  req.user = get[req.headers[`x-forwarded-for`]].info;
+                  req.guilds = get[req.headers[`x-forwarded-for`]].guilds;
+                  req.third_party = get[req.headers[`x-forwarded-for`]].connections;
+         } catch (e) {
+                  req.user = null;
+                  req.guilds = null;
+                  req.third_party = null;
+         }
+         if (req.originalUrl != "/") {
+                  next()
+         } else {
+                  res.render("loading.ejs");
+         }
 });
 
-app.get("/", async(req, res) => {
-         res.render(`main.ejs`);
+app.get("/home", async(req, res) => {
+         res.render(`main.ejs`, {
+                  user: req.user
+         });
 })
 .get("/login", async(req, res) => {
-         require("./modules/login")(req, res, client);
+         require("./modules/login")(req, res/*, client*/);
 })
