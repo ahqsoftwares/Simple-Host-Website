0caf2a9daceaa368b8083043c8dbd23f77d0169e
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-06-07 20:50:23 +0530

    checks

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.listen(1876);
-app.use("ejs");
-app.use("views", path.join(__dirname + "pages"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + "pages"));
 
 app.get("/", async(req, res) => {
          res.render(`main.ejs`);
