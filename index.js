97a6dda468ee41fd3f0856b98ab6da03d0f853a8
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-06-07 20:52:31 +0530

    testing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.listen(1876);
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname + "pages"));
+app.set("views", path.join(__dirname, "pages"));
 
 app.get("/", async(req, res) => {
          res.render(`main.ejs`);
